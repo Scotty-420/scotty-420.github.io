817fa5d7e8fdbacd77552e73764432a7438ba071
Scott <isrealscott679@gmail.com>
2019-07-30 14:18:03 +0200

    latest update

---
+++ main.js
@@ -0,0 +1,45 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCU2xw-osuftMbHMwpYpg5vipGhZ8z4jV4",
+    authDomain: "contactform-efede.firebaseapp.com",
+    databaseURL: "https://contactform-efede.firebaseio.com",
+    projectId: "contactform-efede",
+    storageBucket: "contactform-efede.appspot.com",
+    messagingSenderId: "438641737378",
+    appId: "1:438641737378:web:c341522630e55bac"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var messagesRef= firebase.database().ref('messages');
+
+
+//listen for form Submit
+document.getElementById('contactForm').addEventListener('submit',
+submitForm);
+
+//submit form
+function submitForm(e){
+  e.preventDefault();
+
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var message = getInputVal('message');
+
+  saveMessage(name, email, message);
+}
+
+//function to get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+  //save te message to firebase
+  function saveMessage(name, email, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      email: email,
+      message: message,
+    });
+  }
